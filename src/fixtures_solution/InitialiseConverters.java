7f712dbd383204737cfea95cd59d1e51cd92d57d
Nicole Rauch <info@nicole-rauch.de>
2022-03-14 20:45:27 +0100

    BooleanConverter für Boolean und boolean registrieren.

--- src/fixtures_solution/InitialiseConverters.java
+++ src/fixtures_solution/InitialiseConverters.java
@@ -2,6 +2,7 @@ package fixtures_solution;
 
 public class InitialiseConverters {
 	static {
+		fitnesse.slim.converters.ConverterRegistry.addConverter(boolean.class, new BooleanConverter());
 		fitnesse.slim.converters.ConverterRegistry.addConverter(Boolean.class, new BooleanConverter());
 	}
 }
